4f6cd3c45f63dc3b9d8d6f6fbdcfbede293a998b
ANGEL <kensei1823@gmail.com>
2025-10-17 15:13:49 -0400

    uncomplete category service

--- src/app/features/events/services/event.category.service.ts
+++ src/app/features/events/services/event.category.service.ts
@@ -1,10 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { API_CONFIG } from '@core/config/api.config';
+import { EventCategory } from '@core/models/event.models';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EventCategoryService {
+  private readonly http = inject(HttpClient);
 
-  constructor() { }
+  private get baseUrl(): string {
+    return `${API_CONFIG.baseUrl}${API_CONFIG.endpoints.events.eventCategories.base}/Categories`;
+  }
 
+  getAll(): Observable<EventCategory[]> {
+    return this.http.get<EventCategory[]>(`${this.baseUrl}/GetAll`);
+  }
 }
