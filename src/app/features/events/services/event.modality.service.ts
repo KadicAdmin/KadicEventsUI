bf2d927847a91a63adaee26e01c378378fbb6644
Jeremias Rosario <jeremiasdiaz977@gmail.com>
2025-10-17 16:05:34 -0400

    Refactor EventModalityService by removing unused Modality import and associated comments for cleaner code; streamline the getAll method by eliminating unnecessary console logging.

--- src/app/features/events/services/event.modality.service.ts
+++ src/app/features/events/services/event.modality.service.ts
@@ -1,21 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { API_CONFIG } from '@core/config/api.config';
-import { Modality, ApiResponse, EventModality } from '@core/models';
-import { Observable, of } from 'rxjs';
+import { ApiResponse, EventModality } from '@core/models';
+import { Observable } from 'rxjs';
 
-/**
- * Servicio para gestionar las modalidades de eventos
- * 
- * @example
- * // En tu componente:
- * import { extractData } from '@core/utils/api-response.utils';
- * 
- * modalities = toSignal(
- *   this.modalityService.getAll().pipe(extractData()),
- *   { initialValue: [] }
- * );
- */
 @Injectable({
   providedIn: 'root',
 })
@@ -28,7 +16,6 @@ export class EventModalityService {
 
   getAll(): Observable<ApiResponse<EventModality[]>> {
     const url = `${this.baseUrl}${API_CONFIG.endpoints.events.modality.getAll}`;
-    console.log(url);
     return this.http.get<ApiResponse<EventModality[]>>(url);
   }
 }
