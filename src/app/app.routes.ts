0ebc5edba3a943eb7da4c4659abd44582018dc98
ANGEL <kensei1823@gmail.com>
2025-10-30 16:26:58 -0400

    molecule of upload was created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { ImageFileUpload } from '@shared/components/molecules/image-file-upload/image-file-upload';
 // import { authGuard, adminGuard, guestGuard } from './core'; // Temporalmente desactivado
 
 export const routes: Routes = [
@@ -57,27 +58,32 @@ export const routes: Routes = [
   },
 
   {
+    path: 'upload file',
+    component: ImageFileUpload,
+  },
+
+  {
     path: 'productcard',
     loadComponent: () =>
-      import('@shared/components/molecules/card/product-card-component/product.card.component').then(
-        (m) => m.ProductCardComponent
-      ),
+      import(
+        '@shared/components/molecules/card/product-card-component/product.card.component'
+      ).then((m) => m.ProductCardComponent),
   },
 
   {
     path: 'event cards',
     loadComponent: () =>
-      import('@shared/components/organisms/event-card.component/event.card.component.component').then(
-        (m) => m.CardsListComponent
-      ),
+      import(
+        '@shared/components/organisms/event-card.component/event.card.component.component'
+      ).then((m) => m.CardsListComponent),
   },
 
   {
     path: 'category-event',
     loadComponent: () =>
-      import('@shared/components/organisms/category-card/category-card.component').then(
-        (m) => m.CategoryCardComponent
-      ),
+      import(
+        '@shared/components/organisms/category-card/category-card.component'
+      ).then((m) => m.CategoryCardComponent),
   },
 
   {
