cfd8062d74e14b5038629f8e6081e2cecf76b78d
ANGEL <kensei1823@gmail.com>
2025-11-07 10:27:45 -0400

    ...

--- src/app/shared/components/organisms/image-galery/image-galery.ts
+++ src/app/shared/components/organisms/image-galery/image-galery.ts
@@ -1,41 +1,43 @@
 import { Component, model } from '@angular/core';
 import { PhotoService } from 'app/features/events/services/photoservice';
+import { GalleriaModule } from 'primeng/galleria';
 
 @Component({
   selector: 'app-image-galery',
-  imports: [],
+  imports: [GalleriaModule],
   templateUrl: './image-galery.html',
+  standalone: true,
 })
-export class ImageGalery {
+export class ImageGallery {
   displayCustom: boolean | undefined;
 
-    activeIndex: number = 0;
+  activeIndex: number = 0;
 
-    images = model([]);
+  images = model([]);
 
-    responsiveOptions: any[] = [
-        {
-            breakpoint: '1024px',
-            numVisible: 5
-        },
-        {
-            breakpoint: '768px',
-            numVisible: 3
-        },
-        {
-            breakpoint: '560px',
-            numVisible: 1
-        }
-    ];
+  responsiveOptions: any[] = [
+    {
+      breakpoint: '1024px',
+      numVisible: 5,
+    },
+    {
+      breakpoint: '768px',
+      numVisible: 3,
+    },
+    {
+      breakpoint: '560px',
+      numVisible: 1,
+    },
+  ];
 
-    constructor(private photoService: PhotoService) {}
+  constructor(private photoService: PhotoService) {}
 
-    ngOnInit() {
-        this.photoService.getImages().then((images) => this.images.set(images));
-    }
+  ngOnInit() {
+    this.photoService.getImages().then((images) => this.images.set(images));
+  }
 
-    imageClick(index: number) {
-        this.activeIndex = index;
-        this.displayCustom = true;
-    }
- }
+  imageClick(index: number) {
+    this.activeIndex = index;
+    this.displayCustom = true;
+  }
+}
